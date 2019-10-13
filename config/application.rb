de201e3c7c027385ace06cf4761389b35efe2d1b
yuji sugaya <yujibebobe.jiangu.yousi@gmail.com>
2019-10-13 18:27:45 +0900

    setting config files

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,16 @@ module RailsKadai2
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+
+
+    config.generators do |g|
+      # この二行の記述で自動生成しない設定を作成しています。
+      g.assets false
+      g.helper false
+    end
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
