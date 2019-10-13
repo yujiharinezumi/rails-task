ba701ba2c819884b4a056eaa5890d3d47bbc29bf
yuji sugaya <yujibebobe.jiangu.yousi@gmail.com>
2019-10-13 17:38:44 +0900

    added session function

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   resources:blogs do
     collection do
       post :confirm
     end
   end
    resources :users
+   resources :sessions, only: [:new, :create, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
