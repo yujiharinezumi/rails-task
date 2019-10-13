4d21bbac90ad09308d6dbdd878c2642e3c27d3c8
yuji sugaya <yujibebobe.jiangu.yousi@gmail.com>
2019-10-13 17:02:25 +0900

    added destroy function

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -35,6 +35,11 @@ class BlogsController < ApplicationController
    end
   end
 
+  def destroy
+   @blog.destroy
+   redirect_to blogs_path, notice:"ブログを削除しました！"
+  end
+
   private
 
   def blog_params
