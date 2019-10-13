a5f2c0865ab90b4e2b321b3c485f400774a2e20d
yuji sugaya <yujibebobe.jiangu.yousi@gmail.com>
2019-10-13 20:04:31 +0900

    added index favorite page

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,4 +1,10 @@
 class FavoritesController < ApplicationController
+
+  def index
+    @favorite = current_user.favorites
+  end
+
+
   def create
    favorite = current_user.favorites.create(blog_id: params[:blog_id])
    redirect_to blogs_url, notice: "#{favorite.blog.user.name}さんのブログをお気に入り登録しました"
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   end
    resources :users
    resources :sessions, only: [:new, :create, :destroy]
-   resources :favorites, only: [:create, :destroy]
+   resources :favorites
 
    mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
