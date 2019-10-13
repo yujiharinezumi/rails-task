222ac8ea00e64fad0260c8bb8e84487ab386f44f
yuji sugaya <yujibebobe.jiangu.yousi@gmail.com>
2019-10-13 16:56:42 +0900

    added edit function

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -4,26 +4,38 @@ class BlogsController < ApplicationController
   end
 
   def new
-    @blog = Blog.new
+      @blog = Blog.new
   end
 
   def create
-     Blog.create(blog_params)
-
-     redirect_to blogs_path
+    @blog = Blog.new(blog_params)
+      if @blog.save
+        redirect_to blogs_path, notice: "投稿しました！"
+      else
+        render :new
+      end
   end
 
   def show
-     @blog = Blog.find(params[:id])
+
   end
 
   def edit
+     @blog = Blog.find(params[:id])
   end
 
+  def update
+    @blog = Blog.find(params[:id])
+   if @blog.update(blog_params)
+     redirect_to blogs_path, notice: "ブログを編集しました！"
+   else
+     render :edit
+   end
+  end
 
   private
 
   def blog_params
-    params.require(:blog).permit(:image,:content,:image_cach)
+    params.require(:blog).permit(:image, :image_cache, :content)
   end
 end
--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,3 +1,6 @@
 class Blog < ApplicationRecord
    mount_uploader :image, ImageUploader
+
+   validates :image, presence: true
+   validates :content, presence: true
 end
