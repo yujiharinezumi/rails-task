2a693a2e82c6bf537f5d1e9910a351153576fda9
yuji sugaya <yujibebobe.jiangu.yousi@gmail.com>
2019-12-11 11:35:56 +0900

    recommit

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -7,7 +7,7 @@ CarrierWave.configure do |config|
   config.fog_provider = 'fog/aws'
   config.fog_credentials = {
     provider:                 'AWS',
-    aws_access_ke_id:         ENV["AWS_ACCESS_KEY_ID"] ,
+    aws_access_key_id:         ENV["AWS_ACCESS_KEY_ID"] ,
     aws_secret_access_key:    ENV["AWS_SECRET_ACCESS_KEY"],
     region:                   'ap-northeast-1',
     path_style:               true,
